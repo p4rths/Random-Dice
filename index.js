a01fe150779a31f699d578fb999088005ddffd7a
P4rths <p4rths@gmail.com>
2025-02-01 11:40:25 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1 * 7;
+randomNumber1 = Math.floor(randomNumber1);
+
+var attribute = document.querySelector("img").setAttribute("src" , "./images/dice1.png")
